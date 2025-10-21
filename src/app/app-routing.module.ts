5e2e17bd324efa90aa8773e9e23f8cdcb82955ab
Kaltum Abdala Viveros Gómez <viverosgomezk@gmail.com>
2025-10-21 10:12:52 -0600

    routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { Component, NgModule } from '@angular/core';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class AppRoutingModule { }
